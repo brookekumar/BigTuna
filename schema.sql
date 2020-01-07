f3d0165096c0130db69ef0ffd6012ca848b1945d
Johnny Yang <jyang108@gmail.com>
2020-01-06 19:35:58 -0600

    updated user table to reflect user necessary user data

--- schema.sql
+++ schema.sql
@@ -8,22 +8,14 @@ CREATE TABLE users (
     username VARCHAR(255) NOT NULL,
     user_password VARCHAR(255) NOT NULL,
     user_email VARCHAR(600) NOT NULL,
-    profiles_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (profiles_id) REFERENCES profiles(id)
-);
-
-CREATE TABLE profiles (
-    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    industry_id INT NOT NULL,
-    zip_code INT  NOT NULL,
-    remote_work BOOLEAN DEFAULT 0,
+    scores_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (scores_id) REFERENCES scores(id)
+);
+
+CREATE TABLE scores (
 
 );
 
-CREATE TABLE industries (
-    id INT NOT NULL AUTO_INCRMENT,
-    industry VARCHAR(255) NOT NULL
-)
--- server.js
+++ server.js
@@ -27,12 +27,6 @@ require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
 
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
